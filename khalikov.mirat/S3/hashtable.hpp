4e599d7315f064dbe7170d3e6878c2e3e665a1f0
khalii21 <miratkhalikov007@gmail.com>
2026-05-25 20:14:11 +0300

    hashtable start impl

--- khalikov.mirat/S3/hashtable.hpp
+++ khalikov.mirat/S3/hashtable.hpp
@@ -20,7 +20,11 @@ namespace khalikov
       HashTable& operator=(const HashTable& rhs);
       HashTable& operator=(HashTable&& rhs) noexcept;
       Value& operator[](const Key& key);
+
+      void swap(HashTable& rhs) noexcept;
+
     private:
+      void rehash(const size_t new_cap);
       struct Slot {
         Key key;
         Value value;
@@ -29,5 +33,68 @@ namespace khalikov
       size_t size_;
       size_t cap_;
       Slot* slots_;
+      Hash hasher;
   };
+}
+
+template< class Key, class Value, class Hash, class Equal >
+khalikov::HashTable< Key, Value, Hash, Equal >::HashTable() :
+  size_(0),
+  cap_(8),
+  slots_(new Slot[8])
+{}
+
+template< class Key, class Value, class Hash, class Equal >
+khalikov::HashTable< Key, Value, Hash, Equal >::~HashTable() {
+  delete[] slots_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+void khalikov::HashTable< Key, Value, Hash, Equal >::swap(HashTable& rhs) noexcept {
+  std::swap(size_, rhs.size_);
+  std::swap(cap_, rhs.cap_);
+  std::swap(slots_, rhs.slots_);
+  std::swap(hasher, rhs.hasher);
+}
+
+template< class Key, class Value, class Hash, class Equal >
+khalikov::HashTable< Key, Value, Hash, Equal >::HashTable(const HashTable& rhs) :
+  size_(0),
+  cap_(rhs.cap_),
+  slots_(new Slot[rhs.cap_])
+{
+  for (size_t i = 0; i < rhs.cap_; ++i) {
+    slots_[i].state = rhs.slots_[i].state;
+    if (rhs.slots_[i].state == SlotState::OCCUPIED) {
+      slots_[i].key = rhs.slots_[i].key;
+      slots_[i].value = rhs.slots_[i].value;
+      size_++;
+    }
+  }
+}
+
+template< class Key, class Value, class Hash, class Equal >
+khalikov::HashTable< Key, Value, Hash, Equal >::HashTable(HashTable&& rhs) noexcept :
+  size_(0),
+  cap_(0),
+  slots_(nullptr)
+{
+  this->swap(rhs);
+}
+
+template< class Key, class Value, class Hash, class Equal >
+khalikov::HashTable< Key, Value, Hash, Equal >&
+khalikov::HashTable< Key, Value, Hash, Equal >::operator=(const HashTable& rhs) {
+  if (this != std::addressof(rhs)) {
+    HashTable temp(rhs);
+    this->swap(temp);
+  }
+  return *this;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+khalikov::HashTable< Key, Value, Hash, Equal >&
+khalikov::HashTable< Key, Value, Hash, Equal >::operator=(HashTable&& rhs) noexcept {
+   this->swap(rhs);
+   return *this;
 }
