230418856dc44da451996a4987284ff8d8e0cc96
khalii21 <miratkhalikov007@gmail.com>
2026-06-19 23:25:57 +0300

    iter impl

--- khalikov.mirat/F0/RBIt.hpp
+++ khalikov.mirat/F0/RBIt.hpp
@@ -8,7 +8,6 @@ namespace khalikov {
   struct RBIt
   {
     RBIt();
-    ~RBIt();
     explicit RBIt(TreeNode< T, Cmp > *node);
 
     T &operator*();
@@ -25,6 +24,69 @@ namespace khalikov {
   };
 }
 
+template< class T, class Cmp >
+khalikov::RBIt< T, Cmp >::RBIt():
+  curr(nullptr)
+{}
 
+template< class T, class Cmp >
+khalikov::RBIt< T, Cmp >::RBIt(TreeNode< T, Cmp > *node):
+  curr(node)
+{}
+
+template< class T, class Cmp >
+T &khalikov::RBIt< T, Cmp >::operator*()
+{
+  return curr->data;
+}
+
+template< class T, class Cmp >
+T *khalikov::RBIt< T, Cmp >::operator->()
+{
+  return &(curr->data);
+}
+
+template< class T, class Cmp >
+bool khalikov::RBIt< T, Cmp >::operator==(const RBIt &rhs) const noexcept
+{
+  return curr == rhs.curr;
+}
+
+template< class T, class Cmp >
+bool khalikov::RBIt< T, Cmp >::operator!=(const RBIt &rhs) const noexcept
+{
+  return curr != rhs.curr;
+}
+
+template< class T, class Cmp >
+khalikov::RBIt< T, Cmp > &khalikov::RBIt< T, Cmp >::operator++()
+{
+  if (!curr) {
+    return *this;
+  }
+  if (curr->right) {
+    curr = curr->right;
+    while (curr->left) {
+      curr = curr->left;
+    }
+  }
+  else {
+    TreeNode< T, Cmp > *p = curr->parent;
+    while (p && curr == p->right) {
+      curr = p;
+      p = p->parent;
+    }
+    curr = p;
+  }
+  return *this;
+}
+
+template< class T, class Cmp >
+khalikov::RBIt< T, Cmp > khalikov::RBIt< T, Cmp >::operator++(int)
+{
+  RBIt temp = *this;
+  ++(*this);
+  return temp;
+}
 
 #endif
