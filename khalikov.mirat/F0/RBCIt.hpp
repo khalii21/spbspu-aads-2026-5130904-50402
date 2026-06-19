139a6f982fbe83620f1e04d03b026703719e0ca6
khalii21 <miratkhalikov007@gmail.com>
2026-06-19 23:35:28 +0300

    citer impl

--- khalikov.mirat/F0/RBCIt.hpp
+++ khalikov.mirat/F0/RBCIt.hpp
@@ -2,16 +2,13 @@
 #define RBCIT_HPP
 
 #include "TreeNode.hpp"
-#include "RBIt.hpp"
 
 namespace khalikov {
   template< class T, class Cmp >
   struct RBCIt
   {
     RBCIt();
-    ~RBCIt();
     explicit RBCIt(TreeNode< T, Cmp > *node);
-    explicit RBCIt(const RBIt< T, Cmp > &other);
 
     const T &operator*() const;
     const T *operator->() const;
@@ -19,12 +16,77 @@ namespace khalikov {
     bool operator==(const RBCIt &rhs) const noexcept;
     bool operator!=(const RBCIt &rhs) const noexcept;
 
-    const RBCIt &operator++();
-    const RBCIt operator++(int);
+    RBCIt &operator++();
+    RBCIt operator++(int);
 
     private:
       TreeNode< T, Cmp > *curr;
   };
+}
+
+template< class T, class Cmp >
+khalikov::RBCIt< T, Cmp >::RBCIt():
+  curr(nullptr)
+{}
+
+template< class T, class Cmp >
+khalikov::RBCIt< T, Cmp >::RBCIt(TreeNode< T, Cmp > *node):
+  curr(node)
+{}
+
+template< class T, class Cmp >
+const T &khalikov::RBCIt< T, Cmp >::operator*() const
+{
+  return curr->data;
+}
+
+template< class T, class Cmp >
+const T *khalikov::RBCIt< T, Cmp >::operator->() const
+{
+  return &(curr->data);
+}
+
+template< class T, class Cmp >
+bool khalikov::RBCIt< T, Cmp >::operator==(const RBCIt &rhs) const noexcept
+{
+  return curr == rhs.curr;
+}
+
+template< class T, class Cmp >
+bool khalikov::RBCIt< T, Cmp >::operator!=(const RBCIt &rhs) const noexcept
+{
+  return curr != rhs.curr;
+}
+
+template< class T, class Cmp >
+khalikov::RBCIt< T, Cmp > &khalikov::RBCIt< T, Cmp >::operator++()
+{
+  if (!curr) {
+    return *this;
+  }
+  if (curr->right) {
+    curr = curr->right;
+    while (curr->left) {
+      curr = curr->left;
+    }
+  }
+  else {
+    TreeNode< T, Cmp > *p = curr->parent;
+    while (p && curr == p->right) {
+      curr = p;
+      p = p->parent;
+    }
+    curr = p;
+  }
+  return *this;
+}
+
+template< class T, class Cmp >
+khalikov::RBCIt< T, Cmp > khalikov::RBCIt< T, Cmp >::operator++(int)
+{
+  RBCIt temp = *this;
+  ++(*this);
+  return temp;
 }
 
 #endif
