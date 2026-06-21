56d819e150bd5202037551a31b9e02ddea280245
khalii21 <miratkhalikov007@gmail.com>
2026-06-21 15:27:11 +0300

    baseimpl

--- khalikov.mirat/F0/matrix.hpp
+++ khalikov.mirat/F0/matrix.hpp
@@ -28,6 +28,9 @@ namespace khalikov
     void setBase(int newBase);
     void swap(Matrix &other) noexcept;
 
+    long long stol(const std::string &str, int base);
+    std::string ltos(long long num, int base);
+    static void check(long long a, long long b, char op);
 
   private:
     size_t rows_;
@@ -35,6 +38,118 @@ namespace khalikov
     int base_;
     Vector< Vector< long long > > mx_;
   };
+}
+
+void khalikov::Matrix::check(long long a, long long b, char op)
+{
+  switch (op) {
+    case '+': {
+      if ((b > 0) && (a > std::numeric_limits<long long>::max() - b)) {
+        throw std::overflow_error("Overflow");
+      }
+      if ((b < 0) && (a < std::numeric_limits<long long>::min() - b)) {
+        throw std::underflow_error("Underflow");
+      }
+      break;
+    }
+    case '-': {
+      if ((b < 0) && (a > std::numeric_limits<long long>::max() + b)) {
+        throw std::overflow_error("Overflow");
+      }
+      if ((b > 0) && (a < std::numeric_limits<long long>::min() + b)) {
+        throw std::underflow_error("Underflow");
+      }
+      break;
+    }
+    case '*': {
+      if (a == 0 || b == 0) {
+        return;
+      }
+      if (((a > 0 && b > 0) && (a > std::numeric_limits<long long>::max() / b)) ||
+          ((a < 0 && b < 0) && (b < std::numeric_limits<long long>::max() / a))) {
+        throw std::overflow_error("Overflow");
+      }
+      if (((a > 0 && b < 0) && (b < std::numeric_limits<long long>::min() / a)) ||
+          ((a < 0 && b > 0) && (a < std::numeric_limits<long long>::min() / b))) {
+        throw std::underflow_error("Underflow");
+      }
+      break;
+    }
+    default:
+      throw std::invalid_argument("Unknown op");
+  }
+}
+
+long long khalikov::stol(const std::string &str, int base)
+{
+  if (str.empty()) {
+    throw std::invalid_argument("Empty string");
+  }
+  long long res = 0;
+  bool isNegative = (str[0] == '-');
+  size_t i = (isNegative || str[0] == '+') ? 1 : 0;
+  if (i == str.size()) {
+    throw std::invalid_argument("Invalid number format");
+  }
+  for (; i < str.size(); ++i) {
+    char ch = str[i];
+    int digit = 0;
+    switch (ch) {
+      case '0': case '1': case '2': case '3': case '4':
+      case '5': case '6': case '7': case '8': case '9':
+        digit = ch - '0';
+        break;
+      case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
+      case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
+      case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
+      case 'V': case 'W': case 'X': case 'Y': case 'Z':
+        digit = ch - 'A' + 10;
+        break;
+      case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
+      case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+      case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
+      case 'v': case 'w': case 'x': case 'y': case 'z':
+        digit = ch - 'a' + 10;
+        break;
+      default:
+        throw std::invalid_argument("Invalid character");
+    }
+    if (digit >= base) {
+      throw std::invalid_argument("Input error");
+    }
+    check(res, base, '*');
+    res *= base;
+    check(res, digit, '+');
+    res += digit;
+  }
+  return isNegative ? -res : res;
+}
+
+std::string khalikov::Matrix::ltos(long long num, int base)
+{
+  if (num == 0) {
+    return "0";
+  }
+  std::string result = "";
+  bool isNegative = (num < 0);
+  size_t absoluteNum = isNegative ? -num : num;
+  while (absoluteNum > 0) {
+    int remainder = absoluteNum % base;
+    if (remainder < 10) {
+      result += (remainder + '0');
+    } else {
+      result += (remainder - 10 + 'A');
+    }
+    absoluteNum /= base;
+  }
+  if (isNegative) {
+    result += '-';
+  }
+  size_t n = result.length();
+  for (size_t i = 0; i < n / 2; ++i) {
+    std::swap(result[i], result[n - i - 1]);
+  }
+  return result;
 }
 
 void khalikov::Matrix::swap(Matrix &other) noexcept
@@ -132,5 +247,7 @@ void khalikov::Matrix::setBase(int newBase)
 {
   base_ = newBase;
 }
+
+
 
 #endif
